5f50ecfb18942d874ac359bc848e2726dc86d705
Artem Gunkin <art.gunkin@gmail.com>
2017-07-02 15:22:20 +0300

    Change chart data to get

--- lib/poloniex.rb
+++ lib/poloniex.rb
@@ -55,6 +55,10 @@ module Poloniex
     get 'returnCurrencies'
   end
 
+  def self.chart_data( currency_pair, start = 0, end_time = Time.now.to_i, period = 900 )
+    get 'returnChartData', currencyPair: currency_pair, start: start, end: end_time, period: period
+  end
+
   def self.complete_balances
     post 'returnCompleteBalances'
   end
@@ -65,10 +69,6 @@ module Poloniex
 
   def self.trade_history( currency_pair, start = 0, end_time = Time.now.to_i )
     post 'returnTradeHistory', currencyPair: currency_pair, start: start, :end => end_time
-  end
-
-  def self.chart_data( currency_pair, start = 0, end_time = Time.now.to_i, period = 900 )
-    post 'returnChartData', currencyPair: currency_pair, start: start, :end => end_time, :period => period
   end
 
   def self.buy( currency_pair, rate, amount )
